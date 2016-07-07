809b0036a5973e99898f9a770c57fe5b631004ed
ZhongWen Li <lizw@primeton.com>
2016-07-07 11:16:39 +0800

    UPDATE

--- build.sh
+++ build.sh
@@ -9,16 +9,40 @@
 
 CONTEXT_PATH=$(cd $(dirname ${0}); pwd)
 
-REGISTRY_URL="registry.primeton.com"
+# parse args
+while [ -n "${1}" ]; do
+  case "${1}" in
+  --REGISTRY_URL) REGISTRY_URL="${2}"; shift 2;;
+  --PUSH_IMG) PUSH_IMG="${2}"; shift 2;;
+  --RM_IMG) RM_IMG="${2}"; shift 2;;
+  --LATEST) LATEST="${2}"; shift 2;;
+  *) echo "Skip key ${1}"; shift 1;;
+  esac
+done
 
-PUSH_IMG="no"
-RM_IMG="no"
-LATEST="no"
+if [ -z "${REGISTRY_URL}" ]; then
+    REGISTRY_URL="euler-registry.primeton.com"
+fi
+if [ -z "${PUSH_IMG}" ]; then
+    PUSH_IMG="no"
+fi
+if [ -z "${RM_IMG}" ]; then
+    RM_IMG="no"
+fi
+if [ -z "${LATEST}" ]; then
+    LATEST="no"
+fi
 
 PARAMS="--REGISTRY_URL ${REGISTRY_URL} --PUSH_IMG ${PUSH_IMG} --RM_IMG ${RM_IMG} --LATEST ${LATEST}"
 
-/bin/bash ${CONTEXT_PATH}/jre8/build.sh ${PARAMS}
+# /bin/bash ${CONTEXT_PATH}/jre7/build.sh ${PARAMS}
+
+# /bin/bash ${CONTEXT_PATH}/jdk7/build.sh ${PARAMS}
+
+# /bin/bash ${CONTEXT_PATH}/jre8/build.sh ${PARAMS}
 
 /bin/bash ${CONTEXT_PATH}/jdk8/build.sh ${PARAMS}
 
-/bin/bash ${CONTEXT_PATH}/tomcat8/build.sh ${PARAMS}
+/bin/bash ${CONTEXT_PATH}/jenkins/build.sh ${PARAMS}
+
+# /bin/bash ${CONTEXT_PATH}/tomcat8/build.sh ${PARAMS}
